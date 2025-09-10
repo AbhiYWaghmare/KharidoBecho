98e7f88b30ca6d5f8a508d3b66fd4b3643f64fa0
Abhishek Waghmare <abhishekwaghmare@gtasterix.com>
2025-09-10 17:52:52 +0530

    New Project Added

---
+++ src/main/java/com/spring/jwt/entity/Buyer.java
@@ -0,0 +1,35 @@
+package com.spring.jwt.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "buyers")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Buyer {
+
+    // Primary key is the same as User.user_id (shared PK)
+    @Id
+    @Column(name = "user_id")
+    private Integer id;
+
+    @OneToOne(fetch = FetchType.EAGER, optional = false)
+    @MapsId // use the same primary key value as User
+    @JoinColumn(name = "user_id", referencedColumnName = "user_id", nullable = false, unique = true)
+    @JsonIgnoreProperties({
+            "password",
+            "resetPasswordToken",
+            "resetPasswordTokenExpiry",
+            "deviceFingerprint",
+            "lastLogin",
+            "loginAttempts",
+            "accountLockedUntil",
+            "roles" // hide roles here to keep response minimal; remove if you want them
+    })
+    private User user;
+}
