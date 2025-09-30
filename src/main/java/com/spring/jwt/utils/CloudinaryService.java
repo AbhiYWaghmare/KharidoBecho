853bc8f7996b82addfbe918c92e6552d80b62cea
AbhiYWaghmare <abhiwaghmare40@gmail.com>
2025-09-30 21:00:21 +0530

    Image size with in 400kb can upload and it will compress the image if it more than 400kb

--- src/main/java/com/spring/jwt/utils/CloudinaryService.java
+++ src/main/java/com/spring/jwt/utils/CloudinaryService.java
@@ -24,27 +24,32 @@ public class CloudinaryService {
                 "api_secret", apiSecret));
     }
 
-    /**
-     * Uploads file to Cloudinary and returns only the secure URL (for backward compatibility).
-     */
+    //===================================================================================================================================================//
+    //Uploads file to Cloudinary and returns only the secure URL & compress the image get in bytes then upload(for backward compatibility).
+
+//    public String uploadFile(byte[] data, String folder) throws IOException {
+//        Map uploadResult = cloudinary.uploader().upload(data, ObjectUtils.asMap("folder", folder));
+//        return uploadResult.get("secure_url").toString();
+//    }
+
+    //======================================================================================================================================================//
+
     public String uploadFile(MultipartFile file, String folder) throws IOException {
         Map uploadResult = cloudinary.uploader().upload(file.getBytes(),
                 ObjectUtils.asMap("folder", folder));
         return uploadResult.get("secure_url").toString();
     }
 
-    /**
-     *  Uploads file and returns the full response Map so we can store public_id + URL.
-     */
+    //Uploads file and returns the full response Map so we can store public_id & URL.
+
     public Map<String, Object> uploadFileWithResult(MultipartFile file, String folder) throws IOException {
         return cloudinary.uploader().upload(file.getBytes(),
                 ObjectUtils.asMap("folder", folder));
     }
 
-    /**
-     *  Deletes file from Cloudinary using its public_id.
-     * Returns true if successfully deleted or already not found.
-     */
+    //Deletes file from Cloudinary using its public_id.
+    // Returns true if successfully deleted or already not found.
+
     public boolean deleteByPublicId(String publicId) throws IOException {
         Map result = cloudinary.uploader().destroy(publicId, ObjectUtils.asMap("invalidate", true));
         Object res = result.get("result");
