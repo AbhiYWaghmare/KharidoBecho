8f2ec68d9ba87baa48df547f2e894b2e348a79d1
darshan181101 <darshanshirole@gtasterix.com>
2025-10-30 12:42:50 +0530

    Removed old Bike exception classes from com.spring.jwt.Bike.Exceptions

--- src/main/java/com/spring/jwt/Bike/Exceptions/ResourceNotFound.java
+++
@@ -1,9 +0,0 @@
-package com.spring.jwt.Bike.Exceptions;
-
-public class ResourceNotFound extends RuntimeException
-{
-    public ResourceNotFound(String message)
-    {
-        super(message);
-    }
-}
