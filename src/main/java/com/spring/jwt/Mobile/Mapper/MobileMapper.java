87d4e8d2b22b1bd42e200d83755a9132b5ffa033
AbhiYWaghmare <abhiwaghmare40@gmail.com>
2025-12-24 16:06:14 +0530

    All Changes Completed in mobile

--- src/main/java/com/spring/jwt/Mobile/Mapper/MobileMapper.java
+++ src/main/java/com/spring/jwt/Mobile/Mapper/MobileMapper.java
@@ -3,6 +3,7 @@ package com.spring.jwt.Mobile.Mapper;
 import com.spring.jwt.Mobile.dto.MobileImageDTO;
 import com.spring.jwt.Mobile.dto.MobileRequestDTO;
 import com.spring.jwt.Mobile.dto.MobileResponseDTO;
+import com.spring.jwt.Mobile.dto.MobileUpdateDTO;
 import com.spring.jwt.Mobile.entity.Mobile;
 import com.spring.jwt.Mobile.entity.MobileImage;
 
@@ -44,15 +45,51 @@ public class MobileMapper {
         return dto;
     }
 
-    public static void updateFromRequest(Mobile m, MobileRequestDTO r) {
-        if (r.getTitle() != null) m.setTitle(r.getTitle());
-        if (r.getDescription() != null) m.setDescription(r.getDescription());
-        if (r.getPrice() != null) m.setPrice(r.getPrice());
-        if (r.getNegotiable() != null) m.setNegotiable(r.getNegotiable());
-        if (r.getCondition() != null) m.setCondition(Mobile.Condition.valueOf(r.getCondition()));
-        if (r.getBrand() != null) m.setBrand(r.getBrand());
-        if (r.getModel() != null) m.setModel(r.getModel());
-        if (r.getColor() != null) m.setColor(r.getColor());
-        if (r.getYearOfPurchase() != null) m.setYearOfPurchase(r.getYearOfPurchase());
+//    public static void updateFromRequest(Mobile m, MobileUpdateDTO r) {
+//        if (r.getTitle() != null) m.setTitle(r.getTitle());
+//        if (r.getDescription() != null) m.setDescription(r.getDescription());
+//        if (r.getPrice() != null) m.setPrice(r.getPrice());
+//        if (r.getNegotiable() != null) m.setNegotiable(r.getNegotiable());
+//        if (r.getCondition() != null) m.setCondition(Mobile.Condition.valueOf(r.getCondition()));
+//        if (r.getBrand() != null) m.setBrand(r.getBrand());
+//        if (r.getModel() != null) m.setModel(r.getModel());
+//        if (r.getColor() != null) m.setColor(r.getColor());
+//        if (r.getYearOfPurchase() != null) m.setYearOfPurchase(r.getYearOfPurchase());
+//    }
+
+    public static void updateFromRequest(Mobile m, MobileRequestDTO req) {
+
+        m.setTitle(req.getTitle());
+        m.setDescription(req.getDescription());
+        m.setPrice(req.getPrice());
+        m.setNegotiable(req.getNegotiable());
+        m.setBrand(req.getBrand());
+        m.setModel(req.getModel());
+        m.setColor(req.getColor());
+        if (req.getCondition() != null) {
+            m.setCondition(Mobile.Condition.valueOf(req.getCondition().toUpperCase()));
+        }
+
+        m.setYearOfPurchase(req.getYearOfPurchase());
     }
+
+    public static void updateFromRequest(Mobile m, MobileUpdateDTO req) {
+
+        if (req.getTitle() != null) m.setTitle(req.getTitle());
+        if (req.getDescription() != null) m.setDescription(req.getDescription());
+        if (req.getPrice() != null) m.setPrice(req.getPrice());
+        if (req.getNegotiable() != null) m.setNegotiable(req.getNegotiable());
+        if (req.getBrand() != null) m.setBrand(req.getBrand());
+        if (req.getModel() != null) m.setModel(req.getModel());
+        if (req.getColor() != null) m.setColor(req.getColor());
+
+        if (req.getCondition() != null) {
+            m.setCondition(Mobile.Condition.valueOf(req.getCondition().toUpperCase()));
+        }
+
+        if (req.getYearOfPurchase() != null) {
+            m.setYearOfPurchase(req.getYearOfPurchase());
+        }
+    }
+
 }
